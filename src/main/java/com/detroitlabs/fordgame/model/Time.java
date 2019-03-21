c9cae6431ca6e7ead50db1487ebe34f9172fb67f
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-21 09:38:35 -0400

    create time class and methods, need to change time method values based on use

--- src/main/java/com/detroitlabs/fordgame/model/Time.java
+++ src/main/java/com/detroitlabs/fordgame/model/Time.java
@@ -5,5 +5,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class Time {
 
+    private int time = 100;
+
+    public Time() {
+
+    }
+
+    public void subtractTimeForQuiz() {
+        time -= 1;
+    }
+
+    public void subtractTimeForBossBattle() {
+        time -= 1;
+    }
+
+    public void subtractTimeForAction() {
+        time -= 1;
+    }
+
+    public void addTimeForCorrectQuizAnswer() {
+        time += 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" +
+                "time=" + time +
+                '}';
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
 
 }
