6be2e42c923b73891a559397f10be578582c71f4
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-21 18:37:20 -0400

    able to save and retreive data from db, had to add timezone to application properties to fix errors, need to add security to db

--- src/main/java/com/detroitlabs/hello/AppConfig.java
+++ src/main/java/com/detroitlabs/hello/AppConfig.java
@@ -1,15 +1,13 @@
 package com.detroitlabs.hello;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class AppConfig {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AppConfig.class, args);
     }
+
 }
