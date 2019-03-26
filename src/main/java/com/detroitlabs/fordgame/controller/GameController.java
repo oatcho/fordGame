dc5ea286540fcc6e8efe256b447d75d20f8cbb6d
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-03-26 13:51:15 -0400

    adds dynamic display of question on quiz1 html page, adds method to randomly pick question to be displayed in quiz repo class

--- src/main/java/com/detroitlabs/fordgame/controller/GameController.java
+++ src/main/java/com/detroitlabs/fordgame/controller/GameController.java
@@ -1,8 +1,10 @@
 package com.detroitlabs.fordgame.controller;
 
+import com.detroitlabs.fordgame.data.QuizRepository;
 import com.detroitlabs.fordgame.model.Pokemon;
 import com.detroitlabs.fordgame.model.PokemonMoves;
 import com.detroitlabs.fordgame.model.PokemonSprite;
+import com.detroitlabs.fordgame.model.Question;
 import com.detroitlabs.fordgame.service.Pokemonservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,33 +19,58 @@ public class GameController {
     @Autowired
     Pokemonservice pokemonservice;
 
+//    @RequestMapping("/")
+//    public String displayPokemon(ModelMap modelMap) {
+//        Pokemon pikachu = pokemonservice.fetchSinglePokemon(25);
+////        Pokemon graveler
+//
+//        Pokemon graveler = pokemonservice.fetchSinglePokemon(75);
+//
+//        PokemonSprite pokemonSprite = pikachu.getPokemonSprite();
+//        List<PokemonMoves> pokemonMoves = pikachu.getPokemonMoves();
+//        String moveName = pokemonMoves.get(0).getMoves().getName();
+//
+//        List<PokemonMoves> gravelerMoves = graveler.getPokemonMoves();
+//        String gravelerMoveName = pokemonMoves.get(0).getMoves().getName();
+//
+//
+//        modelMap.put("pikachuSprite", pokemonSprite);
+//        modelMap.put("pikachuName", pikachu.getName());
+////        modelMap.put("move", moveName);
+//        modelMap.put("pikachuMove", pikachu.getPokemonMoves());
+//        modelMap.put("pikachuWeight", pikachu.getWeight());
+//        modelMap.put("pikachuBaseExperience", pikachu.getBase_experience());
+//        modelMap.put("pikachuId", pikachu.getId());
+//
+//        modelMap.put("gravelerName", graveler.getName());
+//        modelMap.put("gravelerSprite", graveler.getPokemonSprite());
+//        modelMap.put("gravelerMove", gravelerMoveName);
+//
+//        return "start";
+//    }
+
     @RequestMapping("/")
-    public String displayPokemon(ModelMap modelMap) {
+    public String displayStartPage(ModelMap modelMap) {
+        setPlayerPokemonDetails(modelMap);
+        return "start";
+    }
+
+    @RequestMapping("/quizOne")
+    public String displayFirstQuizPage(ModelMap modelMap, QuizRepository quizRepository) {
+       setPlayerPokemonDetails(modelMap);
+       String question = QuizRepository.ALL_TRUE_FALSE_QUESTIONS.get(quizRepository.generateRandomNumberForTfQuestion()).getQuestion();
+       modelMap.put("tfQuestion", question);
+        return "quiz1";
+    }
+
+    private void setPlayerPokemonDetails(ModelMap modelMap) {
         Pokemon pikachu = pokemonservice.fetchSinglePokemon(25);
-//        Pokemon graveler
-
-        Pokemon graveler = pokemonservice.fetchSinglePokemon(75);
-
         PokemonSprite pokemonSprite = pikachu.getPokemonSprite();
-        List<PokemonMoves> pokemonMoves = pikachu.getPokemonMoves();
-        String moveName = pokemonMoves.get(0).getMoves().getName();
-
-        List<PokemonMoves> gravelerMoves = graveler.getPokemonMoves();
-        String gravelerMoveName = pokemonMoves.get(0).getMoves().getName();
-
-
         modelMap.put("pikachuSprite", pokemonSprite);
         modelMap.put("pikachuName", pikachu.getName());
-//        modelMap.put("move", moveName);
         modelMap.put("pikachuMove", pikachu.getPokemonMoves());
         modelMap.put("pikachuWeight", pikachu.getWeight());
         modelMap.put("pikachuBaseExperience", pikachu.getBase_experience());
         modelMap.put("pikachuId", pikachu.getId());
-
-        modelMap.put("gravelerName", graveler.getName());
-        modelMap.put("gravelerSprite", graveler.getPokemonSprite());
-        modelMap.put("gravelerMove", gravelerMoveName);
-
-        return "start";
     }
 }
