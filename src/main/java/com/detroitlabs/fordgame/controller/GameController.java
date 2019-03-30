9ea234eee74b691cb1bae9d4d8b0408e600e617a
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-30 18:34:11 -0400

    add user registration page, able to create, save, & login with new user from cloud sql, succesful login brings you to start of game

--- src/main/java/com/detroitlabs/fordgame/controller/GameController.java
+++ src/main/java/com/detroitlabs/fordgame/controller/GameController.java
@@ -1,19 +1,19 @@
 package com.detroitlabs.fordgame.controller;
 
 import com.detroitlabs.fordgame.data.QuizRepository;
-import com.detroitlabs.fordgame.model.Pokemon;
-import com.detroitlabs.fordgame.model.PokemonSprite;
-import com.detroitlabs.fordgame.model.Question;
-import com.detroitlabs.fordgame.model.Time;
+import com.detroitlabs.fordgame.model.*;
 import com.detroitlabs.fordgame.service.Pokemonservice;
 import com.detroitlabs.fordgame.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class GameController {
@@ -27,14 +27,45 @@ public class GameController {
     @Autowired
     UserService userService;
 
+    // **** User Auth **** //
     @RequestMapping(value={"/login"}, method = RequestMethod.GET)
-    public String login(@RequestParam(value = "param.error", required = false) String error) {
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+        ModelAndView modelAndView = new ModelAndView();
         if (error != null) {
-            return "error-page";
-        } else {
-            return "login";
-        }
+            modelAndView.setViewName("error-page");
+        } else modelAndView.setViewName("login");
+        return modelAndView;
     }
+
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("registration");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("registration");
+        }
+        return modelAndView;
+    }
+    // **** **** //
 
     // **** Start Page **** //
     @RequestMapping("/")
