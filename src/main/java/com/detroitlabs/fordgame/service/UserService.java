c8559b3fde72d7d6de71d6318086098355bf036a
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-30 14:04:22 -0400

    add user service

---
+++ src/main/java/com/detroitlabs/fordgame/service/UserService.java
@@ -0,0 +1,42 @@
+package com.detroitlabs.fordgame.service;
+
+import com.detroitlabs.fordgame.model.Role;
+import com.detroitlabs.fordgame.model.User;
+import com.detroitlabs.fordgame.repository.RoleRepository;
+import com.detroitlabs.fordgame.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service("userService")
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+
+}
